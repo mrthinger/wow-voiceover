4a9cefd0833debebd3843e92e8b17b65bc7573b3
xvwyh <43143822+xvwyh@users.noreply.github.com>
2023-04-08 18:40:39 +0300

    Tabs to spaces

--- AI_VoiceOver/Debug.lua
+++ AI_VoiceOver/Debug.lua
@@ -3,14 +3,14 @@ VoiceOverDebug = {}
 VoiceOverDebug.__index = Debug
 
 function VoiceOverDebug:print(msg, header)
-	if (Addon.db.profile.main.DebugEnabled) then
-		if (header) then
-			print(VoiceOverUtils:colorizeText("VoiceOver", VoiceOverUtils.ColorCodes.DefaultGold) ..
-				VoiceOverUtils:colorizeText("( " .. header .. ")", VoiceOverUtils.ColorCodes.Grey) ..
-				" - " .. msg)
-		else
-			print(VoiceOverUtils:colorizeText("VoiceOver", VoiceOverUtils.ColorCodes.DefaultGold) ..
-				" - " .. msg)
-		end
-	end
+    if (Addon.db.profile.main.DebugEnabled) then
+        if (header) then
+            print(VoiceOverUtils:colorizeText("VoiceOver", VoiceOverUtils.ColorCodes.DefaultGold) ..
+                VoiceOverUtils:colorizeText("( " .. header .. ")", VoiceOverUtils.ColorCodes.Grey) ..
+                " - " .. msg)
+        else
+            print(VoiceOverUtils:colorizeText("VoiceOver", VoiceOverUtils.ColorCodes.DefaultGold) ..
+                " - " .. msg)
+        end
+    end
 end
