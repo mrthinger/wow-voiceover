ca432ad2b1cf9978f8a3608ddd658f7ca76a27c0
xvwyh <43143822+xvwyh@users.noreply.github.com>
2023-04-11 22:39:38 +0300

    Sync Compatibility.lua

--- AI_VoiceOver/Compatibility.lua
+++ AI_VoiceOver/Compatibility.lua
@@ -1,6 +1,62 @@
 setfenv(1, VoiceOver)
 
-local CLIENT_VERSION = GetBuildInfo()
+local CLIENT_VERSION, _, _, INTERFACE_VERSION = GetBuildInfo()
+INTERFACE_VERSION = INTERFACE_VERSION or 0 -- 1.12 doesn't return this
+
+if not select then
+    function _G.select(index, ...)
+        local result = {}
+        for i = index, arg.n do
+            table.insert(result, arg[i])
+        end
+        return unpack(result)
+    end
+end
+
+if not print then
+    function _G.print(...)
+        local text = ""
+        for i = 1, arg.n do
+            text = text .. (i > 1 and " " or "") .. tostring(arg[i])
+        end
+        DEFAULT_CHAT_FRAME:AddMessage(text)
+    end
+end
+
+if not strsplit then
+    function strsplit(delimiter, text)
+        local result = {}
+        local from = 1
+        local delim_from, delim_to = string.find(text, delimiter, from)
+        while delim_from do
+            table.insert(result, string.sub(text, from, delim_from - 1))
+            from = delim_to + 1
+            delim_from, delim_to = string.find(text, delimiter, from)
+        end
+        table.insert(result, string.sub(text, from))
+        return unpack(result)
+    end
+end
+
+if not string.gmatch then
+    string.gmatch = string.gfind
+end
+
+if not GetAddOnEnableState then
+    function GetAddOnEnableState(character, addon)
+        addon = addon or character -- GetAddOnEnableState([character], addon)
+        local name, _, _, _, loadable, reason = _G.GetAddOnInfo(addon)
+        if not name or not loadable and reason == "DISABLED" then
+            return 0
+        end
+        return 2
+    end
+
+    function GetAddOnInfo(indexOrName)
+        local name, title, notes, enabled, loadable, reason, security, newVersion = _G.GetAddOnInfo(indexOrName)
+        return name, title, notes, loadable, reason, security, newVersion
+    end
+end
 
 if not GetQuestID then
     local source, text
@@ -21,8 +77,8 @@ if not GetQuestID then
     end
 end
 
-function CreateFrame(frameType, name, parent, template, ...)
-    local frame = _G.CreateFrame(frameType, name, parent, template, ...)
+function CreateFrame(frameType, name, parent, template)
+    local frame = _G.CreateFrame(frameType, name, parent, template)
     if not frame.SetResizeBounds then
         function frame:SetResizeBounds(minWidth, minHeight, maxWidth, maxHeight)
             frame:SetMinResize(minWidth, minHeight)
@@ -34,7 +90,19 @@ function CreateFrame(frameType, name, parent, template, ...)
     return frame
 end
 
-if not WOW_PROJECT_ID and CLIENT_VERSION == "1.12.1" then
+if not WOW_PROJECT_ID and string.sub(CLIENT_VERSION, 1, 4) == "1.12" then
+
+    function Utils:GetNPCName()
+        return UnitName("npc")
+    end
+
+    function Utils:GetNPCGUID()
+        return nil
+    end
+
+    function Utils:IsNPCPlayer()
+        return UnitIsPlayer("npc")
+    end
 
 elseif not WOW_PROJECT_ID and CLIENT_VERSION == "3.3.5" then
 
--- AI_VoiceOver_112/Compatibility.lua
+++ AI_VoiceOver_112/Compatibility.lua
@@ -77,7 +77,21 @@ if not GetQuestID then
     end
 end
 
+function CreateFrame(frameType, name, parent, template)
+    local frame = _G.CreateFrame(frameType, name, parent, template)
+    if not frame.SetResizeBounds then
+        function frame:SetResizeBounds(minWidth, minHeight, maxWidth, maxHeight)
+            frame:SetMinResize(minWidth, minHeight)
+            if maxWidth and maxHeight then
+                frame:SetMaxResize(maxWidth, maxHeight)
+            end
+        end
+    end
+    return frame
+end
+
 if not WOW_PROJECT_ID and string.sub(CLIENT_VERSION, 1, 4) == "1.12" then
+
     function Utils:GetNPCName()
         return UnitName("npc")
     end
@@ -89,4 +103,36 @@ if not WOW_PROJECT_ID and string.sub(CLIENT_VERSION, 1, 4) == "1.12" then
     function Utils:IsNPCPlayer()
         return UnitIsPlayer("npc")
     end
+
+elseif not WOW_PROJECT_ID and CLIENT_VERSION == "3.3.5" then
+
+elseif WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then
+
+elseif WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC then
+
+    GetGossipText = C_GossipInfo.GetText
+    GetNumGossipActiveQuests = C_GossipInfo.GetNumActiveQuests
+    GetNumGossipAvailableQuests = C_GossipInfo.GetNumAvailableQuests
+
+    function Utils:GetQuestLogScrollOffset()
+        return HybridScrollFrame_GetOffset(QuestLogListScrollFrame)
+    end
+
+    function Utils:GetQuestLogTitleFrame(index)
+        return _G["QuestLogListScrollFrameButton" .. index]
+    end
+
+    function QuestOverlayUI:UpdateQuestTitle(questLogTitleFrame, playButton, normalText, questCheck)
+        playButton:SetPoint("LEFT", questLogTitleFrame.normalText, "LEFT", 4, 0)
+        questLogTitleFrame.normalText:SetText([[|TInterface\AddOns\AI_VoiceOver\Textures\spacer:1:24|t]] ..
+            (questLogTitleFrame.normalText:GetText() or ""):trim())
+        QuestLogTitleButton_Resize(questLogTitleFrame)
+    end
+
+    hooksecurefunc(Addon, "OnInitialize", function()
+        QuestLogListScrollFrame.update = QuestLog_Update
+    end)
+
+elseif WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
+
 end
